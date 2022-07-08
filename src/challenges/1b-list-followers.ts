d05c489e15ab3e7cb75c05499ff496e6db6eb3e1
Bhawick Jain <bhawick@gmail.com>
2022-07-08 07:16:43 +0100

    correct bugs reported by jest

--- src/challenges/1b-list-followers.ts
+++ src/challenges/1b-list-followers.ts
@@ -1,7 +1,14 @@
 function listFollowers(followerNames: string[]): string {
   // complete function
   // use array destructuring, for practice!
-  return "fails test right now";
+
+  const [firstFollower, secondFollower, ...theRest] = followerNames
+  
+  if (followerNames.length === 2) {
+    return `Followed by ${firstFollower} and ${secondFollower}`;
+  }
+
+  return `Followed by ${firstFollower}, ${secondFollower} and others`;
 }
 
 export default listFollowers;
